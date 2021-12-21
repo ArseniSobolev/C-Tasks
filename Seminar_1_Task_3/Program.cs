f822c151064bf953ddd5b62ef4bce7b98653ef99
ArseniS <arsenal17@gmail.com>
2021-12-21 16:33:01 +0300

    Seminar 1 task 3

---
+++ Seminar_1_Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter number ");
+string numberA = Console.ReadLine();
+int number1 = int.Parse(numberA);
+
+if (number1 % 2 == 0)
+{
+  Console.WriteLine("Yes");
+}
+else
+{
+  Console.WriteLine("No");
+}
