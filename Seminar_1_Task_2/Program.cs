aa68f29a19ae603ad2147e375b95b91f215a218c
ArseniS <arsenal17@gmail.com>
2021-12-21 16:20:34 +0300

    Seminar 1 task 2

---
+++ Seminar_1_Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("enter number N ");
+string numberN = Console.ReadLine();
+int number = int.Parse(numberN);
+int chetnye = 2;
+
+while (chetnye <= number)
+{
+    if (chetnye % 2 == 0)
+    {
+        Console.WriteLine(chetnye);
+    }
+    chetnye = chetnye + 1;
+}
